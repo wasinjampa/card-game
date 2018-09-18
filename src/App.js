2078fee707a707a76e0995d6ca618f24083c4705
wasinjampa <wasin28919@gmail.com>
2018-09-18 22:06:40 +0700

    Components & Props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import CharacterCard from './CharacterCard'; 
+import WordCard from './WordCard';
 import './App.css';
-  class App extends Component {
-  x = "Hello"
-  y = "{\"World\"}"
+
+const word = "Hello";
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        {this.x + this.y}
+      <div>
+        {<WordCard value="hello"/>}
       </div>
       );
     }
---
+++ src/CharacterCard.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class CharacterCard extends Component {
+    render(){
+        return (
+            <div className = 'card'>{this.props.value}</div>
+    )
+    }
+}
