4313a42af0a4670084db0f03dd01ceae154770ff
wasinjampa <wasin28919@gmail.com>
2018-09-21 21:48:09 +0700

    commit ครั้งที่ 8

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        {<WordCard value="princeofsongkla"/>}
+      {<WordCard value="princeofsongkla"/>}
       </div>
       );
     }
